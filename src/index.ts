89f1c100748fb20291832b4d94becf6d73a50b3f
Kiril Krsteski <kkrsteski@plume.com>
2024-05-19 18:28:50 +0200

    fix: export CarouselRef

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import Carousel, { CarouselProps } from "./Carousel";
+import Carousel, { CarouselProps, CarouselRef } from "./Carousel";
 import CarouselArrows, { CarouselArrowsProps } from "./CarouselArrows";
 import CarouselDots, { CarouselDotsProps } from "./CarouselDots";
 
 export { Carousel, CarouselArrows, CarouselDots };
-export type { CarouselArrowsProps, CarouselDotsProps, CarouselProps };
+export type {
+  CarouselArrowsProps,
+  CarouselDotsProps,
+  CarouselProps,
+  CarouselRef,
+};
